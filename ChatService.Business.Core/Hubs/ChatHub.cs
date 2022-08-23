99bcf919c3593dbcf48708328e0f586defeadb23
Sergio Bermudez <sergio97bermudez@gmail.com>
2022-08-23 12:29:28 -0500

    First Commit

---
+++ ChatService.Business.Core/Hubs/ChatHub.cs
@@ -0,0 +1,54 @@
+﻿using ChatService.Domain.Data.Hub;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Business.Core.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string botUser;
+        private readonly IDictionary<string, UserConnection> connections;
+
+        public ChatHub(IDictionary<string, UserConnection> connections)
+        {
+            botUser = "SerberChat Bot";
+            this.connections = connections;
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            connections[Context.ConnectionId] = userConnection;
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", botUser,
+                $"{userConnection.User} has joined {userConnection.Room}");
+
+            await SendConnectedUsers(userConnection.Room);
+        }
+
+        public override async Task<Task> OnDisconnectedAsync(Exception exception)
+        {
+            if (connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                connections.Remove(Context.ConnectionId);
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", botUser, 
+                    $"{userConnection.User} has left");
+
+                await SendConnectedUsers(userConnection.Room);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+
+        public async Task SendConnectedUsers(string room)
+        {
+            var users = connections.Values.Where(c => c.Room == room).Select(c=>c.User);
+            await Clients.Group(room).SendAsync("UsersInRoom", users);
+        }
+    }
+}
---
+++ ChatService/Startup.cs
@@ -0,0 +1,65 @@
+﻿using ChatService.Business.Core.Hubs;
+using ChatService.Domain.Data.Hub;
+
+namespace ChatService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<SerberChatContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddScoped<ISerberChatRepository, SerberChatRepository>();
+            //services.AddControllers();
+            services.AddSignalR();
+            //services.AddSwaggerGen(c =>
+            //{
+            //	c.SwaggerDoc("v1", new OpenApiInfo { Title = "SerberChat.Api", Version = "v1" });
+            //});
+            services.AddCors(options =>
+            {
+                options.AddPolicy("ClientPermission", policy =>
+                {
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000")
+                        .AllowCredentials();
+                });
+            });
+
+            services.AddSingleton<IDictionary<string, UserConnection>>(opts => new Dictionary<string, UserConnection>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SerberChat.Api v1"));
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseCors("ClientPermission");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                    //endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
